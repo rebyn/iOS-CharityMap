b4d47f25a16172a2caeb4874a993a15ffc228a07
Tu Hoang <rebyn@me.com>
2014-07-31 03:31:36 +0700

    added app icons

--- Charity Map/activitiesViewController.m
+++ Charity Map/activitiesViewController.m
@@ -20,16 +20,17 @@
     self.discoverMoreButton.layer.cornerRadius = 5;
     self.discoverMoreButton.clipsToBounds = YES;
     
+    // Status bar to have the same background as navigationBar
     UIView *syncedStatusBarBackground = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 20)];
     syncedStatusBarBackground.backgroundColor = [UIColor colorWithRed:0.973 green:0.973 blue:0.973 alpha:1]; /*#f8f8f8*/
     [self.view addSubview:syncedStatusBarBackground];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    // Turn on empty
-    // [self.emptyActivitiesWelcomeScreen removeFromSuperview];
-    [UIView animateWithDuration:5 animations:^{
-        _emptyActivitiesWelcomeScreen.alpha = 0.1;
+    // Fancy fade-in animation at the beginning
+    _emptyActivitiesWelcomeScreen.alpha = 0;
+    [UIView animateWithDuration:1.5 animations:^{
+        _emptyActivitiesWelcomeScreen.alpha = 1;
     }];
 }
 
